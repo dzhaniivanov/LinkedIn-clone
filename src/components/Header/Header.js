360e62f2ad6567ed4460284e2922098b376f4666
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-05-11 18:12:06 +0300

    added navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,6 +17,56 @@ const Header = (props) => {
                         <img src="/images/search-icon.svg" alt="" />
                     </SearchIcon>
                 </Search>
+                <Nav>
+                    <NavListWrap>
+                        <NavList className='active'>
+                            <a>
+                                <img src="/images/nav-home.svg" alt="" />
+                                <span>Home</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-network.svg" alt="" />
+                                <span>My Network</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-jobs.svg" alt="" />
+                                <span>Jobs</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-messaging.svg" alt="" />
+                                <span>Messaging</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-notifications.svg" alt="" />
+                                <span>Notifications</span>
+                            </a>
+                        </NavList>
+                        <User>
+                            <a>
+                                <img src="/images/user.svg" alt="" />
+                                <span>Me</span>
+                                <img src="/images/down-icon.svg" alt="" />
+                            </a>
+                        </User>
+                        <Work>
+                            <a>
+                                <img src="/images/nav-work.svg" alt="" />
+                                <span>Work
+                                    <img src="/images/down-icon.svg" alt="" />
+                                </span>
+                            </a>
+                        </Work>
+
+                    </NavListWrap>
+                </Nav>
             </Content>
         </Container>
     )
@@ -83,6 +133,83 @@ const SearchIcon = styled.div`
     display:flex;
     justify-content:center;
     align-items:center;
-`
+`;
+
+const Nav = styled.nav`
+    margin-left:auto;
+    display:block;
+
+    @media(max-width:768px){
+        position:fixed;
+        left:0;
+        bottom:0;
+        background:white;
+        width:100%;
+    };
+`;
+
+const NavListWrap = styled.ul`
+    display:flex;
+    flex-wrap:nowrap;
+    list-style-type:none;
+
+    .active {
+        span:after {
+            content:'';
+            transform:scaleX(1);
+            border-bottom:2px solid var(--white,#fff);
+            bottom:0;
+            top:0;
+            left:0;
+            position:absolute;
+            transition:transform 0.2s ease-in-out;
+            width:100%;
+            border-color:rgba(0,0,0,0.9);
+        }
+    }
+
+`;
+
+const NavList = styled.li`
+    display:flex;
+    align-items:center;
+
+    a { 
+        align-items:center;
+        background:transperent;
+        display:flex;
+        flex-direction:column;
+        font-size:12px;
+        font-weight:400;
+        justify-content:center;
+        line-height:1.5;
+        min-height:42px;
+        min-width:80px;
+        position:relative;
+        text-decoration:none;
+
+        span {
+            color:rgba(0,0,0,0.6);
+            display:flex;
+            align-items:center;
+        }
+
+        @media(max-width:768px){
+            min-width:70px;
+        }
+    }
+
+    &:hover,&:active{
+        a{
+            span{
+                color:rgba(0,0,0,0.9);
+            }
+        }
+    }
+`;
+
+const User = styled(NavList)``;
+
+const Work = styled(User)``;
 
 export default Header;
