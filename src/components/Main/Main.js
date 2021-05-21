81620cf741a027e94db49cfccd979ea3d6f52a8c
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-05-21 21:02:38 +0300

    main page updated

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,32 +3,33 @@ import styled from 'styled-components';
 const Main = (props) => {
     return (
         <Container>
-            <ShareBox>Share</ShareBox>
+            <ShareBox>Share
             <div>
-                <img src="/images/user.svg" alt="" />
-                <button>Start a post</button>
-            </div>
-            <div>
-                <button>
-                    <img src="/images/photo-icon.svg" alt="" />
-                    <span>Photo</span>
-                </button>
+                    <img src="/images/user.svg" alt="" />
+                    <button>Start a post</button>
+                </div>
+                <div>
+                    <button>
+                        <img src="/images/photo-icon.svg" alt="" />
+                        <span>Photo</span>
+                    </button>
 
-                <button>
-                    <img src="/images/video-icon.svg" alt="" />
-                    <span>Video</span>
-                </button>
+                    <button>
+                        <img src="/images/video-icon.svg" alt="" />
+                        <span>Video</span>
+                    </button>
 
-                <button>
-                    <img src="/images/event-icon.svg" alt="" />
-                    <span>Event</span>
-                </button>
+                    <button>
+                        <img src="/images/event-icon.svg" alt="" />
+                        <span>Event</span>
+                    </button>
 
-                <button>
-                    <img src="/images/article-icon.svg" alt="" />
-                    <span>Write post</span>
-                </button>
-            </div>
+                    <button>
+                        <img src="/images/article-icon.svg" alt="" />
+                        <span>Write article</span>
+                    </button>
+                </div>
+            </ShareBox>
         </Container>
     )
 
@@ -55,7 +56,54 @@ const ShareBox = styled(CommonCard)`
     color:#958b7b;
     margin:0 0 8px;
     background:white;
+    div {
+        button {
+            outline:none;
+            color:rgba(0,0,0,0.6);
+            font-size:14px;
+            line-height:1.5;
+            min-height:48px;
+            background:transparent;
+            border:none;
+            display:flex;
+            align-items:center;
+            font-weight:600;
+        }
+        &:first-child { 
+            display:flex;
+            align-items:center;
+            padding:8px 16px 0px 16px;
+            img {
+                width:48px;
+                border-radius:50%;
+                margin-right:8px;
+            }
+            button {
+                margin:4px 0;
+                flex-grow:1;
+                border-radius:35px;
+                padding-left:16px;
+                border:1px solid rgba(0,0,0,0.15);
+                background-color: white;
+                text-align:left;
+            }
+        }
+        &:nth-child(2) {
+            display:flex;
+            flex-wrap:wrap;
+            justify-content:space-around;
+            padding-bottom:4px;
 
+            button  { 
+                img { 
+                    margin:0 4px 0 -2px;
+                }
+                span {
+                    color:#70b5f9;
+                }
+            }
+        }
+    }
 `;
 
 export default Main;
