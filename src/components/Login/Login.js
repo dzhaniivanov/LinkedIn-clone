dab8e3169a001efd77d899ac3ffc819fa50a4b57
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-05-27 17:51:14 +0300

    added google sign up with popup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 import { connect } from 'react-redux';
+import { signInAPI } from '../../actions';
+import { Redirect } from 'react-router-dom';
 
 const Login = (props) => {
     return (
         <Container>
+            {
+                props.user &&
+                <Redirect to='/home' />
+            }
             <Nav>
                 <a href="/">
                     <img src="/images/login-logo.svg" alt="" />
@@ -23,7 +29,7 @@ const Login = (props) => {
                     <img src="/images/login-hero.svg" alt="" />
                 </Hero>
                 <Form>
-                    <Google>
+                    <Google onClick={() => props.signIn()}>
                         <img src="/images/google.svg" alt="" />
                         Sign in with Google
                     </Google>
@@ -180,9 +186,13 @@ const Google = styled.button`
 `;
 
 const mapStateToProps = (state) => {
-    return {};
+    return {
+        user:state.userState.user,
+    };
 };
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+    signIn: () => dispatch(signInAPI()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
