049e637875ec9d109d85cdfee1b22e7c08f6ac17
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-05-28 18:16:10 +0300

    styling post modal

--- src/components/PostModal/PostModal.js
+++ src/components/PostModal/PostModal.js
@@ -41,7 +41,27 @@ const Content = styled.div`
 
 `;
 
-const Header = styled.div``;
+const Header = styled.div`
+    display:block;
+    padding:16px 20px;
+    border-bottom:1px solid rgba(0,0,0,0,15);
+    font-size:16px;
+    line-height:1.5;
+    color:rgba(0,0,0,0.6);
+    font-weight:400;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    button {
+        height:40px;
+        width:40px;
+        min-width:auto;
+        color:rgba(0,0,0,0.15);
+        svg {
+            pointer-events:none;
+        }
+    }
+`;
 
 
 export default PostModal;
